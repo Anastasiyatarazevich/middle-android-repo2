3bd6100c0350cf08cf10a5d53bd2c124aa75044d
Anastasia Tarazevich <anastasiya.tarazevich22@gmail.com>
2026-04-02 12:06:25 +0300

    feat: completed task

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,28 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
+import kotlin.math.pow
+
+const val INIT_DELAY = 100L
+const val DELAY = 2.0
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
 
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()  // TODO Задание 2: добавьте обработку ошибок
+        return api.getReply()
+            .retryWhen { cause, attempt ->
+                if (cause is Exception) {
+                    val multiplier = DELAY.pow(attempt.toDouble())
+                    val delayTime = (INIT_DELAY * multiplier).toLong()
+                    delay(delayTime)
+                    true
+                } else {
+                    false
+                }
+            }
     }
 }
